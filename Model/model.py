e134d13dd6e4f8e3329b4f8a668a0c694284f66f
tsengwoody <tsengwoody.tw@gmail.com>
2019-11-20 11:31:49 +0800

    一、開發 model2 的 property 二、使用 test_model.py 對 model 測試

--- Model/model.py
+++ Model/model.py
@@ -1,5 +1,156 @@
-from fakedata import documents
+﻿from fakedata import documents
 from enums import PanelType, ActionType
+from pubsub import pub
+
+
+class Model2:
+	"""
+		每次更新皆需發送以下資料給 UI 顯示，需額外加 index_path 資訊，以便 UI 操作時傳訊給 model
+		current section ：右上當前資料夾內容
+		sections ：左側資料夾樹
+		path ：路徑
+		raw data ：選定物件的原始資料
+		html data ：選定物件的轉換 html 資料
+	"""
+
+	def __init__(self):
+		self.data = documents
+
+		# index_path 放入逐層的索引項來指到特定物件
+		# 最後一項代表選取項目，可為 -1 代表無選取項目
+		self.index_path = [0, 0, 1]
+
+		for index, node in enumerate(self.data):
+			self.set_descendant_index_path(node, [index])
+
+	def get_node_by_index_path(self, index_path):
+		pointer = None
+		for i, index in enumerate(index_path):
+			# 特例，最上層為一個多文件的 list
+			if i == 0:
+				pointer = self.data[i]
+			else:
+				pointer = pointer['items'][index]
+
+		return pointer
+
+	@classmethod
+	def set_descendant_index_path(cls, node, index_path):
+		node['index_path'] = index_path
+		if node['type'] == 'section':
+			for index, child in enumerate(node['items']):
+				child_index_path = index_path + [index]
+				cls.set_descendant_index_path(child, child_index_path)
+
+	@property
+	def current_section(self):
+		"""
+			return: {
+				'index_path': [0,1,1],
+				'data': {...}, # 當前資料夾的資料，需含 index_path
+			}
+		"""
+		return {
+			'index_path': self.index_path[:-1],
+			'data': self.get_node_by_index_path(self.index_path[:-1]),
+		}
+
+	@property
+	def sections(self):
+		"""
+			return: [
+				{
+					'index_path': [0,1,1], # 節點的 index_path
+					'label': '', # 節點的顯示 label
+					'items': [...], # 子節點
+				}, ...
+			]
+		"""
+		pass
+
+	@property
+	def path(self):
+		path = []
+		for i, index in enumerate(self.index_path[:-1]):
+			# 特例，最上層為一個多文件的 list
+			if i == 0:
+				pointer = self.data[i]
+			else:
+				pointer = pointer['items'][index]
+			path.append(pointer['label'])
+
+		return '/'.join(path)
+
+	@property
+	def pointer_raw_data(self):
+		"""
+			return: {
+				'index_path': [0,1,1],
+				'data': {...}, # 選定物件的 raw
+			}
+		"""
+		return {
+			'index_path': self.index_path if not self.index_path[-1] == -1 else None,
+			'data': self.get_node_by_index_path(self.index_path) if not self.index_path[-1] == -1 else None,
+		}
+
+	@property
+	def pointer_html_data(self):
+		"""
+			return: {
+				'index_path': [0,1,1],
+				'data': {...}, # 選定物件的 html
+			}
+		"""
+		return {
+			'index_path': self.index_path if not self.index_path[-1] == -1 else None,
+			'data': self.get_node_by_index_path(self.index_path) if not self.index_path[-1] == -1 else None,
+		}
+
+	# === 操作修改資料 ===
+	def set_index_path(self, data):
+		"""
+			資料格式：data: {
+				'index_path': [0,1,1], # 要放入的位置
+			}
+			判斷：如果 index_path 為不存在的 index 則 raise exception
+		"""
+		self.index_path = data['index_path']
+		for event in ['current_section', 'sections', 'path', 'pointer_raw_data', 'pointer_html_data']:
+			try:
+				pub.sendMessage(event, data=getattr(self, event))
+			except BaseException as e:
+				print('error:', str(e))
+
+	def add(self, data):
+		"""
+			資料格式：data: {
+				'index_path': [0,1,1], # 要放入的位置
+				'data': {...}, # 要放入的資料
+			}
+			判斷：如果 index_path 除最後一個外有不存在的 index 則 raise exception
+		"""
+		pass
+
+	def update(self, data):
+		"""
+			data: {
+				'index_path': [0,1,1], # 要更新的位置
+				'data': {...}, # 要更新的資料
+			}
+			判斷：如果 index_path 有不存在的 index 則 raise exception
+		"""
+		pass
+
+	def remove(self, data):
+		"""
+			data: {
+				'index_path': [0,1,1], # 要刪除的位置
+			}
+			判斷：如果 index_path 有不存在的 index 則 raise exception
+		"""
+		pass
+
 
 # layer: current folder layer number in data
 # index: current item index number in a layer
@@ -87,101 +238,3 @@ class Model:
 				self.controller.sendMessage('data_changedTree', data=self.ori_data)
 			count_data['clickable'] = False
 			self.controller.sendMessage(self.event_name[1], data=count_data)
-
-class Model2:
-	"""
-		每次更新皆需發送以下資料給 UI 顯示，需額外加 index_path 資訊，以便 UI 操作時傳訊給 model
-		current folder ：右上當前資料夾內容
-		folder filter ：左側資料夾樹
-		path ：路徑
-		raw data ：選定物件的原始資料
-		html data ：選定物件的轉換 html 資料
-	"""
-
-	def __init__(self):
-		self.ori_data = documents
-
-		# index_path 放入逐層的索引項來指到特定物件
-		# 最後一項可為 -1 代表無選取項目
-		self.index_path = [0]
-
-	def current_folder(self):
-		"""
-			return: {
-				'index_path': [0,1,1],
-				'data': {...}, # 當前資料夾的資料，需含 index_path
-			}
-		"""
-		pass
-
-	def folder_filter(self):
-		"""
-			return: [
-				{
-					'index_path': [0,1,1], # 節點的 index_path
-					'label': '', # 節點的顯示 label
-					'items': [...], # 子節點
-				}, ...
-			]
-		"""
-		pass
-
-	def path(self):
-		pass
-
-	def raw_data(self):
-		"""
-			return: {
-				'index_path': [0,1,1],
-				'data': {...}, # 選定物件的 raw
-			}
-		"""
-		pass
-
-	def html_data(self):
-		"""
-			return: {
-				'index_path': [0,1,1],
-				'data': {...}, # 選定物件的 html
-			}
-		"""
-		pass
-
-	# === 操作修改資料 ===
-	def set_path(self, data):
-		"""
-			資料格式：data: {
-				'index_path': [0,1,1], # 要放入的位置
-			}
-			判斷：如果 index_path 為不存在的 index 則 raise exception
-		"""
-		self.index_path = data['index_path']
-
-	def add(self, data):
-		"""
-			資料格式：data: {
-				'index_path': [0,1,1], # 要放入的位置
-				'data': {...}, # 要放入的資料
-			}
-			判斷：如果 index_path 除最後一個外有不存在的 index 則 raise exception
-		"""
-		pass
-
-	def update(self, data):
-		"""
-			data: {
-				'index_path': [0,1,1], # 要更新的位置
-				'data': {...}, # 要更新的資料
-			}
-			判斷：如果 index_path 有不存在的 index 則 raise exception
-		"""
-		pass
-
-	def remove(self, data):
-		"""
-			data: {
-				'index_path': [0,1,1], # 要刪除的位置
-			}
-			判斷：如果 index_path 有不存在的 index 則 raise exception
-		"""
-		pass
---
+++ test_model.py
@@ -0,0 +1,38 @@
+﻿from pubsub import pub
+from Model.model import Model2
+
+# 測試是否不同事件會對應不同 method 所用
+def decorate_event(event):
+	def outer_d_f(f):
+		def d_f(data, *args, **kargs):
+			print("%s before call" % (event))
+			result = f(data, *args, **kargs)
+			print("%s after call" % (event))
+			return result
+		return d_f
+	return outer_d_f
+
+def show(data):
+	print(data)
+
+fs = {
+	'current_section': decorate_event('current_section')(show),
+	'sections': decorate_event('sections')(show),
+	'path': decorate_event('path')(show),
+	'pointer_raw_data': decorate_event('pointer_raw_data')(show),
+	'pointer_html_data': decorate_event('pointer_html_data')(show),
+}
+
+model = Model2()
+print(model.index_path)
+print(getattr(model, 'path'))
+
+# 測試改變路徑
+print('before subscribe')
+model.set_index_path({'index_path': [0, 0, 2]})
+
+for event, func in fs.items():
+	pub.subscribe(func, event)
+
+print('after subscribe')
+model.set_index_path({'index_path': [0, 0, 2]})
