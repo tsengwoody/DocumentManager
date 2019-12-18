13b92cac40a3149c59daff84c241a18697419d5c
邱文淇 <ivan.chiou@eitc.com.tw>
2019-12-18 21:29:56 +0800

    整理dialog.py 符合flake8 format

--- View/dialog.py
+++ View/dialog.py
@@ -7,13 +7,13 @@ class NewItemDialog(wx.Dialog):
 		self.parent = parent
 		self.newItemType = "section"
 		self.newItemName = "New Item"
-		self.objectTypes = ["section", "text", "mathml"] 
+		self.objectTypes = ["section", "text", "mathml"]
 
 		# ===============================================================================
 		# UI Control Initialize
 		# ===============================================================================
 		self.panel = wx.Panel(self, wx.ID_ANY)
-		self.box = wx.BoxSizer(wx.VERTICAL) 
+		self.box = wx.BoxSizer(wx.VERTICAL)
 
 		# Select Type
 		self.objectTypesLabel = wx.StaticText(self.panel, label="選擇新增的物件類型?")
@@ -22,7 +22,7 @@ class NewItemDialog(wx.Dialog):
 
 		# Input Name
 		self.nameLabel = wx.StaticText(self.panel, label="輸入物件名稱: ")
-		self.nameTextCtrl = wx.TextCtrl(self.panel, size=(-1,20), value=defaultValue)
+		self.nameTextCtrl = wx.TextCtrl(self.panel, size=(-1, 20), value=defaultValue)
 
 		self.OKBtn = wx.Button(self.panel, -1, label="OK")
 		self.QuitBtn = wx.Button(self.panel, -1, label="Cancel")
@@ -30,18 +30,16 @@ class NewItemDialog(wx.Dialog):
 		# ===============================================================================
 		# Sizer
 		# ===============================================================================
-		self.btn_BSizer = wx.BoxSizer(orient = wx.HORIZONTAL)
+		self.btn_BSizer = wx.BoxSizer(orient=wx.HORIZONTAL)
 		self.btn_BSizer.Add(self.OKBtn, 1, wx.EXPAND, border=5)
 		self.btn_BSizer.Add(self.QuitBtn, 1, wx.EXPAND, border=5)
 
-
-
-		self.all_BSizer = wx.BoxSizer(orient = wx.VERTICAL)
-		self.all_BSizer.Add(self.objectTypesLabel, 1, wx.TOP | wx.BOTTOM| wx.ALL, border=5)
-		self.all_BSizer.Add(self.typeChoice, 1, wx.EXPAND| wx.ALL, border=1)
-		self.all_BSizer.Add(self.nameLabel, 1, wx.TOP | wx.BOTTOM| wx.ALL, border=5)
-		self.all_BSizer.Add(self.nameTextCtrl, 1, wx.EXPAND| wx.ALL, border=1)
-		self.all_BSizer.Add(self.btn_BSizer, 1, wx.EXPAND| wx.ALL, border=1)
+		self.all_BSizer = wx.BoxSizer(orient=wx.VERTICAL)
+		self.all_BSizer.Add(self.objectTypesLabel, 1, wx.TOP | wx.BOTTOM | wx.ALL, border=5)
+		self.all_BSizer.Add(self.typeChoice, 1, wx.EXPAND | wx.ALL, border=1)
+		self.all_BSizer.Add(self.nameLabel, 1, wx.TOP | wx.BOTTOM | wx.ALL, border=5)
+		self.all_BSizer.Add(self.nameTextCtrl, 1, wx.EXPAND | wx.ALL, border=1)
+		self.all_BSizer.Add(self.btn_BSizer, 1, wx.EXPAND | wx.ALL, border=1)
 
 		self.panel.SetSizer(self.all_BSizer)
 
@@ -54,10 +52,10 @@ class NewItemDialog(wx.Dialog):
 
 	def onSelect(self, event):
 		# print(self.combo.GetValue())
-		self.newItemType = self.objectTypes[ self.typeChoice.GetSelection() ]
+		self.newItemType = self.objectTypes[self.typeChoice.GetSelection()]
 
 	def onOK(self, event):
-		self.newItemType = self.objectTypes[ self.typeChoice.GetSelection() ]
+		self.newItemType = self.objectTypes[self.typeChoice.GetSelection()]
 		self.newItemName = self.nameTextCtrl.GetValue().strip()
 		self.EndModal(wx.ID_OK)
 
